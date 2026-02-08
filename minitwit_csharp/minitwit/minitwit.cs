4f4156c06a43057fd34e7d9a9235a6e5835d942a
Mads Nørklit Jensen <macj@itu.com>
2026-02-08 14:07:09 +0100

    Add database connection in C#
    Currently also includes a small hardcoded query to test that connection
    works, should be removed later on
    
    Co-authored-by: Max Brix Koch <mbko@itu.dk>
    Co-authored-by: Carmen Alberte Nielsen <alcn@itu.dk>
    Co-authored-by: Mathilde Julie Gonzalez-Knudsen <mgon@itu.dk>

--- minitwit_csharp/minitwit/minitwit.cs
+++ minitwit_csharp/minitwit/minitwit.cs
@@ -1,36 +1,35 @@
 using Microsoft.Data.Sqlite;
+using MiniTwitns;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-using SqliteConnection connection = new SqliteConnection("Data Source=./minitwit.db");
-
-connection.Open();
-
-using SqliteCommand command = connection.CreateCommand();
+// Hardcoded test to query database for first tweet to verify connection works.
+MiniTwit miniTwit = new MiniTwit();
+using SqliteCommand command = miniTwit.connect_db().CreateCommand();
 command.CommandText = """
-    SELECT text
-    FROM message
-    WHERE message_id = 1
+  SELECT text
+  FROM message
+  WHERE message_id = 1
 """;
 
 using var reader = command.ExecuteReader();
 
 while (reader.Read())
 {
-    Console.WriteLine(reader.GetString(0));
+  Console.WriteLine(reader.GetString(0));
 }
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -44,3 +43,19 @@ app.MapRazorPages()
    .WithStaticAssets();
 
 app.Run();
+
+namespace MiniTwitns
+{
+  class MiniTwit
+  {
+    // Configuration
+    string DATABASE = "./minitwit.db";
+
+    public SqliteConnection connect_db()
+    {
+      SqliteConnection connection = new SqliteConnection($"Data Source= {DATABASE}");
+      connection.Open();
+      return connection;
+    }
+  }
+}
