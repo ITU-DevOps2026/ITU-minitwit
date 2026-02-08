795246dccf50a50fd07c0ab5f9890db31be225a8
MathildeGonzalez <118437175+MathildeGonzalez@users.noreply.github.com>
2026-02-08 17:01:59 +0100

    Query db function (#19)
    * Change gitmessage
    
    * Add method to query database
    
    Co-authored-by: Mads Christian Nørklit Jensen <macj@itu.dk>
    Co-authored-by: Max Brix Koch <mbko@itu.dk>
    Co-authored-by: Carmen Alberte Nielsen <alcn@itu.dk>
    
    * Add razorpages files in lib
    
    * MoreMore razor page files that was ignored
    
    * refactor to two query methods for select and insert
    add test for insert query
    
    Co-authored-by: Mads Christian Nørklit Jensen <macj@itu.dk>
    Co-authored-by: Max Brix Koch <mbko@itu.dk>
    Co-authored-by: Carmen Alberte Nielsen <alcn@itu.dk>
    
    ---------
    
    Co-authored-by: Mads Christian Nørklit Jensen <macj@itu.dk>
    Co-authored-by: Max Brix Koch <mbko@itu.dk>
    Co-authored-by: Carmen Alberte Nielsen <alcn@itu.dk>
    Co-authored-by: Mads Nørklit Jensen <macj@itu.com>

--- minitwit_csharp/minitwit/minitwit.cs
+++ minitwit_csharp/minitwit/minitwit.cs
@@ -5,19 +5,27 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Hardcoded test to query database for first tweet to verify connection works.
 MiniTwit miniTwit = new MiniTwit();
-using SqliteCommand command = miniTwit.connect_db().CreateCommand();
-command.CommandText = """
-  SELECT text
-  FROM message
-  WHERE message_id = 1
-""";
+miniTwit.Connect_db();
+//var param = new SqliteParameter("@Id", 1);
+//var res = miniTwit.Query_db("SELECT * FROM message WHERE message_id = @Id", [param]);
+var res = miniTwit.Query_db_Read("SELECT * FROM message WHERE message_id < 5", []);
 
-using var reader = command.ExecuteReader();
-
-while (reader.Read())
-{
-  Console.WriteLine(reader.GetString(0));
+foreach (Dictionary<string, object> dict in res) {
+  foreach (KeyValuePair<string, object> kvp in dict)
+  {
+      Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+  }
 }
+
+/*var msid = new SqliteParameter("@Message_id", SqliteType.Integer) { Value = 11325 };
+var auid = new SqliteParameter("@Author_id", SqliteType.Integer) { Value = 1 };
+var text = new SqliteParameter("@Text", SqliteType.Text) { Value = "Vi skriver SQL" };
+var pub  = new SqliteParameter("@Pub_date", SqliteType.Integer) { Value = 1769777843 };
+var flag = new SqliteParameter("@Flagged", SqliteType.Integer) { Value = 0 };
+
+var insert = miniTwit.Query_db_Insert("INSERT INTO message (message_id, author_id, text, pub_date, flagged) VALUES (@Message_id, @Author_id, @Text, @Pub_date, @Flagged)", [msid, auid, text, pub, flag]);
+
+Console.WriteLine(insert);*/
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -50,12 +58,56 @@ namespace MiniTwitns
   {
     // Configuration
     string DATABASE = "./minitwit.db";
+    SqliteConnection connection;
 
-    public SqliteConnection connect_db()
+    public SqliteConnection Connect_db()
     {
-      SqliteConnection connection = new SqliteConnection($"Data Source= {DATABASE}");
+      connection = new SqliteConnection($"Data Source= {DATABASE}");
       connection.Open();
       return connection;
+    }
+
+    public List<Dictionary<string, object>> Query_db_Read(string query, SqliteParameter[] args, bool one=false)
+    {
+      return (List<Dictionary<string, object>>) Query_db(query, args, false, one);
+    }
+
+    public int Query_db_Insert(string query, SqliteParameter[] args, bool one=false)
+    {
+      return (int) Query_db(query, args, true, one);
+    }
+
+    public object Query_db(string query, SqliteParameter[] args, bool nonQuery, bool one=false)
+    {
+      SqliteCommand command = connection.CreateCommand();
+      command.CommandText = query;
+      foreach (SqliteParameter param in args)
+      {
+        command.Parameters.Add(param);
+      }
+
+      if (nonQuery)
+      {
+        return command.ExecuteNonQuery();
+      }
+
+      SqliteDataReader reader = command.ExecuteReader();
+
+      List<Dictionary<string, object>> results = new List<Dictionary<string, object>>();
+
+      while (reader.Read())
+      {
+        Dictionary<string, object> row = new Dictionary<string, object>();
+        for (int i = 0; i < reader.FieldCount; i++)
+        {
+          row.Add(reader.GetName(i), reader.GetValue(i));
+        }
+        results.Add(row);
+
+        if (one) break;
+      }
+
+      return results;
     }
   }
 }
