e4d7228d34f5259d20ccf82d6f52eaaa25527cb6
Mathilde Gonzalez-Knudsen <mgon@itu.dk>
2026-02-08 16:00:43 +0100

    Add method to query database
    Co-authored-by: Mads Christian Nørklit Jensen <macj@itu.dk>
    Co-authored-by: Max Brix Koch <mbko@itu.dk>
    Co-authored-by: Carmen Alberte Nielsen <alcn@itu.dk>

--- minitwit_csharp/minitwit/minitwit.cs
+++ minitwit_csharp/minitwit/minitwit.cs
@@ -5,18 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Hardcoded test to query database for first tweet to verify connection works.
 MiniTwit miniTwit = new MiniTwit();
-using SqliteCommand command = miniTwit.connect_db().CreateCommand();
-command.CommandText = """
-  SELECT text
-  FROM message
-  WHERE message_id = 1
-""";
+miniTwit.Connect_db();
+//var param = new SqliteParameter("@Id", 1);
+//var res = miniTwit.Query_db("SELECT * FROM message WHERE message_id = @Id", [param]);
+var res = miniTwit.Query_db("SELECT * FROM message WHERE message_id < 5", []);
 
-using var reader = command.ExecuteReader();
-
-while (reader.Read())
-{
-  Console.WriteLine(reader.GetString(0));
+foreach (Dictionary<string, object> dict in res) {
+  foreach (KeyValuePair<string, object> kvp in dict)
+  {
+      Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+  }
 }
 
 // Add services to the container.
@@ -50,12 +48,41 @@ namespace MiniTwitns
   {
     // Configuration
     string DATABASE = "./minitwit.db";
+    SqliteConnection connection;
 
-    public SqliteConnection connect_db()
+    public SqliteConnection Connect_db()
     {
-      SqliteConnection connection = new SqliteConnection($"Data Source= {DATABASE}");
+      connection = new SqliteConnection($"Data Source= {DATABASE}");
       connection.Open();
       return connection;
+    }
+
+    public List<Dictionary<string, object>> Query_db(string query, SqliteParameter[] args, bool one=false)
+    {
+      SqliteCommand command = connection.CreateCommand();
+      command.CommandText = query;
+      foreach (SqliteParameter param in args)
+      {
+        command.Parameters.Add(param);
+      }
+
+      SqliteDataReader reader = command.ExecuteReader();
+
+      List<Dictionary<string, object>> results = new List<Dictionary<string, object>>();
+
+      while (reader.Read())
+      {
+        Dictionary<string, object> row = new Dictionary<string, object>();
+        for (int i = 0; i < reader.FieldCount; i++)
+        {
+          row.Add(reader.GetName(i), reader.GetValue(i));
+        }
+        results.Add(row);
+
+        if (one) break;
+      }
+
+      return results;
     }
   }
 }
