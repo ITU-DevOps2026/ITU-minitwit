fc3b2ddc2c43bc5f24c0039c6e59d2b6402c9189
Max Brix Koch <mbko@itu.dk>
2026-02-08 22:19:21 +0100

    Fixed hrefs on links

--- minitwit_csharp/minitwit/Pages/PublicTimeline.cshtml.cs
+++ minitwit_csharp/minitwit/Pages/PublicTimeline.cshtml.cs
@@ -7,7 +7,7 @@ namespace minitwit.Pages;
 public class PublicTimelineModel : PageModel
 { 
   public List<Dictionary<string, object>> Messages { get; private set; }
-  public override void OnGet()
+  public void OnGet()
   {
     MiniTwit minitwit = new MiniTwit();
     minitwit.Connect_db();
