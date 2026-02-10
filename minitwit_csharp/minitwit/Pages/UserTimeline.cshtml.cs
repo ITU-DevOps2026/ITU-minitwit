41d5fb621ffcc0c79b42d7ec623d103b1e29d3aa
Max Brix Koch <mbko@itu.dk>
2026-02-10 17:02:23 +0100

    Fixed Is_following so it uses logged in username

--- minitwit_csharp/minitwit/Pages/UserTimeline.cshtml.cs
+++ minitwit_csharp/minitwit/Pages/UserTimeline.cshtml.cs
@@ -7,23 +7,21 @@ public class UserTimelineModel : PageModel
 { 
   public List<Dictionary<string, object>> Messages { get; private set; }
   public bool Followed { get; set; }
-  public IActionResult OnGet(string username)
+  public void OnGet(string username)
   {
 
     // if logged in show users own timeline
     // if not logged in, redirect to public
     string? logged_in_username = HttpContext.Session.GetString("Logged_In_Username");
+    
+    // fetch messages 
+    MiniTwit minitwit = new MiniTwit();
+    minitwit.Connect_db();
+
+    Messages = minitwit.Get_user_timeline(username);
     if(logged_in_username != null)
     {
-      // fetch messages 
-      MiniTwit minitwit = new MiniTwit();
-      minitwit.Connect_db();
-
-      Messages = minitwit.Get_user_timeline(username);
-      Followed = minitwit.Is_followed(logged_in_username, username);
-      return Page();
+      Followed = minitwit.Is_following(logged_in_username, username);
     }
-
-    return RedirectToPage("./PublicTimeline");
   }
 }
