18409143375558e4d9a073c79a98272e560e7fd1
Noerklit <mchrnj@gmail.com>
2026-04-23 23:32:05 +0200

    add missing comma and underscore

--- init_db.sql
+++ init_db.sql
@@ -6,7 +6,7 @@ CREATE TABLE user (
   user_id integer primary key auto_increment,
   username varchar(100) not null,
   email varchar(200) null,
-  pw_hash varchar(200) not null
+  pw_hash varchar(200) not null,
   UNIQUE INDEX index_username (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -14,7 +14,7 @@ CREATE TABLE follower (
   who_id integer,
   whom_id integer,
   primary key (who_id, whom_id),
-  INDEX idx_follower_whom (whom_id),
+  INDEX idx_follower_whom (whom_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE message (
@@ -24,7 +24,7 @@ CREATE TABLE message (
   pub_date integer,
   flagged integer,
   INDEX index_msg_flagged_date (flagged, pub_date DESC),
-  INDEX index msg_author_flagged_date (author_id, flagged, pub_date DESC)
+  INDEX index_msg_author_flagged_date (author_id, flagged, pub_date DESC)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE latest (
